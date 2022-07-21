8bdb7c77bf489434296b97734fe8d36598c36cf1
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2022-07-21 13:25:02 -0700

    Initial Commit

---
+++ Units/UnitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitsApp.swift
+//  Units
+//
+//  Created by Suhail Khan on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
